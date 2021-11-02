eeb9c4753cce765badf63a0e26a3794941fb911b
Brian <brianbtfa@hotmail.com>
2021-11-02 11:58:53 -0600

    validations_query

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from pydantic import BaseModel
 #FastAPI
 from fastapi import FastAPI 
 #Allows to know that a parameter its body type
-from fastapi import Body
+from fastapi import Body, Query
+
+
 
 app = FastAPI()
 
@@ -26,3 +28,13 @@ def home() -> str:
 def create_person(person : Person = Body(...)) -> str:
 
     return '200 OK'
+
+#Validations: Query Parameters
+
+@app.get('/person/detail')
+def show_person(
+    name : Optional[str] = Query(default=None,minlength=1, maxlength=50),
+    #Not recommended to use required with Query parameters
+    age : str = Query(...)
+):
+    return {name : age}
